0cc6dcc0d677bd6c487f894af8d2b45b0f06e95b
PranavChaudhari009 <chaudharipranav175@gmail.com>
2026-05-10 17:13:50 +0530

    Update user.sql

--- treveloop_database/user.sql
+++ treveloop_database/user.sql
@@ -1,11 +1,13 @@
 -- USERS TABLE
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
 
+    country VARCHAR(100),
+    travel_preference VARCHAR(100),
     profile_photo TEXT,
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
