647afd7a97d69b9cda1437afb6d5b6a751b3a6bc
Aastha-tech19 <aashuaastha00@gmail.com>
2026-05-10 14:04:34 +0530

    Create Footer.jsx

---
+++ traveloop-frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer className="bg-slate-900 border-t border-slate-800 py-8 text-center text-gray-400">
+      <h2 className="text-cyan-400 text-2xl font-bold">
+        Traveloop ✈️
+      </h2>
+
+      <p className="mt-2">
+        Personalized Travel Planning Made Easy
+      </p>
+
+      <p className="mt-4 text-sm">
+        © 2026 Traveloop. All rights reserved.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
